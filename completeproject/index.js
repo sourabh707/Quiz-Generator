2e713dc42e8f4bbece3d374454de1b311255f3d3
sourabh707 <85352464+sourabh707@users.noreply.github.com>
2021-09-13 17:48:48 +0530

    Add files via upload

---
+++ completeproject/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')//path module import
+var exphbs = require('express-handlebars')
+const app = express()
+const port = 5010
+
+
+app.engine('handlebars',exphbs());
+app.set('view engine','handlebars');
+
+
+app.use(express.static(path.join(__dirname,"static")))
+
+app.use('/',require(path.join(__dirname,"routes/app.js")))
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
